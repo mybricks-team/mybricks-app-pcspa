ec4821a9dd191e390f6c1786381c49bb4558bed9
GodStream <zpqwangyiyouxiang@163.com>
2023-07-14 15:31:37 +0800

    feat: upgrade comlib

--- pages/src/constants/index.ts
+++ pages/src/constants/index.ts
@@ -1,8 +1,8 @@
 export const COOKIE_LOGIN_USER = "mybricks-login-user";
 
 /** 默认组件库，新页面创建时会使用 */
-export const ComlibEditUrl = 'https://f2.eckwai.com/kos/nlav12333/fangzhou/pub/comlibs/7632_1.2.11/2023-07-12_15-05-08/edit.js'
-export const ComlibRtUrl = 'https://f2.eckwai.com/kos/nlav12333/fangzhou/pub/comlibs/7632_1.2.11/2023-07-12_15-05-08/rt.js'
+export const ComlibEditUrl = 'https://f2.eckwai.com/kos/nlav12333/fangzhou/pub/comlibs/7632_1.2.12/2023-07-13_19-48-00/edit.js'
+export const ComlibRtUrl = 'https://f2.eckwai.com/kos/nlav12333/fangzhou/pub/comlibs/7632_1.2.12/2023-07-13_19-48-00/rt.js'
 
 export const ChartsEditUrl = 'https://f2.eckwai.com/kos/nlav12333/fangzhou/pub/comlibs/5952_1.0.0-main.0/2022-12-06_16-24-51/edit.js'
 export const ChartsRtUrl = 'https://f2.eckwai.com/kos/nlav12333/fangzhou/pub/comlibs/5952_1.0.0-main.0/2022-12-06_16-24-51/rt.js'
