792c869ae0d030d39629d71bea75d602321f943a
GodStream <zpqwangyiyouxiang@163.com>
2023-07-19 16:29:35 +0800

    ref: upgrade

--- pages/src/constants/index.ts
+++ pages/src/constants/index.ts
@@ -1,14 +1,14 @@
 export const COOKIE_LOGIN_USER = "mybricks-login-user";
 
 /** 默认组件库，新页面创建时会使用 */
-export const ComlibEditUrl = 'https://f2.eckwai.com/kos/nlav12333/fangzhou/pub/comlibs/7632_1.2.12/2023-07-13_19-48-00/edit.js'
-export const ComlibRtUrl = 'https://f2.eckwai.com/kos/nlav12333/fangzhou/pub/comlibs/7632_1.2.12/2023-07-13_19-48-00/rt.js'
+export const ComlibEditUrl = 'https://f2.eckwai.com/kos/nlav12333/fangzhou/pub/comlibs/7632_1.2.14/2023-07-19_16-26-43/edit.js'
+export const ComlibRtUrl = 'https://f2.eckwai.com/kos/nlav12333/fangzhou/pub/comlibs/7632_1.2.14/2023-07-19_16-26-43/rt.js'
 
 export const ChartsEditUrl = 'https://f2.eckwai.com/kos/nlav12333/fangzhou/pub/comlibs/5952_1.0.0-main.0/2022-12-06_16-24-51/edit.js'
 export const ChartsRtUrl = 'https://f2.eckwai.com/kos/nlav12333/fangzhou/pub/comlibs/5952_1.0.0-main.0/2022-12-06_16-24-51/rt.js'
 
-export const BasicEditUrl = 'https://f2.eckwai.com/kos/nlav12333/fangzhou/pub/comlibs/7182_1.0.22/2023-07-10_21-31-39/edit.js'
-export const BasicRtUrl = 'https://f2.eckwai.com/kos/nlav12333/fangzhou/pub/comlibs/7182_1.0.22/2023-07-10_21-31-39/rt.js'
+export const BasicEditUrl = 'https://f2.eckwai.com/kos/nlav12333/fangzhou/pub/comlibs/7182_1.0.28/2023-07-19_16-26-08/edit.js'
+export const BasicRtUrl = 'https://f2.eckwai.com/kos/nlav12333/fangzhou/pub/comlibs/7182_1.0.28/2023-07-19_16-26-08/rt.js'
 
 
 /** 发布时使用，根据 edit 获取 rt 资源 */
