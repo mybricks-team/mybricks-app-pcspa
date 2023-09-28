99f8a09a02d38690888742acf89c94f63093fc63
liuzhigang06 <liuzhigang06@kuaishou.com>
2023-09-28 16:16:24 +0800

    fix: fix coonector

--- pages/src/pages/home/app-config.tsx
+++ pages/src/pages/home/app-config.tsx
@@ -492,12 +492,13 @@ export default function (ctx, save, designerRef, remotePlugins = []) {
           } else {
             return plugin.callConnector({ ...connector }, newParams, {
               ...connectorConfig,
-              before: options => {
-                return {
-                  ...options,
-                  url: shapeUrlByEnv(envList, ctx.executeEnv, options.url, ctx.MYBRICKS_HOST)
-                }
-              }
+              before: connector.connectorName === '@mybricks/plugins/service'
+                ? options => {
+                  return {
+                    ...options,
+                    url: shapeUrlByEnv(envList, ctx.executeEnv, options.url, ctx.MYBRICKS_HOST)
+                  }
+                } : null
             });
           }
         },
