407a067445849a77aae86cdc24ec99c4afa94f1f
tangxiaoxin <tangxiaoxin@kuaishou.com>
2023-10-17 20:58:22 +0800

    execChain

--- publish.js
+++ publish.js
@@ -5,61 +5,73 @@ const args = process.argv.slice(2);
 const isOffline = !!args.find((a) => a === "offline");
 const noServiceUpdate = !!args.find((a) => a === "--noServiceUpdate");
 
-const publishReactAppOffline = (callback) => {
-  const buildCommand = `cd pages && npm run build:react-offline`;
-  shelljs.exec(buildCommand, () => {
-    const syncCommand = `node sync_offline.js react`;
-    shelljs.exec(syncCommand, callback);
-  });
+const publishReactAppOffline = () => {
+  return new Promise((resolve) => {
+    const buildCommand = `cd pages && npm run build:react-offline`;
+    shelljs.exec(buildCommand, () => {
+      const syncCommand = `node sync_offline.js react`;
+      shelljs.exec(syncCommand, resolve);
+    });
+  })
+
 };
 
-const publishVue2AppOffline = (callback) => {
-  const buildCommand = `cd pages && npm run build:vue2-offline`;
-  shelljs.exec(buildCommand, () => {
-    const syncCommand = `node sync_offline.js vue2`;
-    shelljs.exec(syncCommand, callback);
-  });
+const publishVue2AppOffline = () => {
+  return new Promise((resolve) => {
+    const buildCommand = `cd pages && npm run build:vue2-offline`;
+    shelljs.exec(buildCommand, () => {
+      const syncCommand = `node sync_offline.js vue2`;
+      shelljs.exec(syncCommand, resolve);
+    });
+  })
 };
 
-const publishReactAppOnline = (callback) => {
-  const buildCommand = `cd pages && npm run build:react`;
-  shelljs.exec(buildCommand, () => {
-    const syncCommand = `npm publish --registry=https://registry.npmjs.org && node sync.js --origin=https://my.mybricks.world react ${
-      noServiceUpdate ? "--noServiceUpdate" : ""
-    }`;
-    shelljs.exec(syncCommand, callback);
-  });
+const publishReactAppOnline = () => {
+  return new Promise((resolve) => {
+    const buildCommand = `cd pages && npm run build:react`;
+    shelljs.exec(buildCommand, () => {
+      const syncCommand = `npm publish --registry=https://registry.npmjs.org && node sync.js --origin=https://my.mybricks.world react ${noServiceUpdate ? "--noServiceUpdate" : ""
+        }`;
+      shelljs.exec(syncCommand, resolve);
+    });
+  })
+
 };
 
-const publishVue2AppOnline = (callback) => {
-  const buildCommand = `cd pages && npm run build:vue2`;
-  shelljs.exec(buildCommand, () => {
-    const syncCommand = `npm publish --registry=https://registry.npmjs.org && node sync.js --origin=https://my.mybricks.world vue2 ${
-      noServiceUpdate ? "--noServiceUpdate" : ""
-    }`;
-    shelljs.exec(syncCommand, callback);
-  });
+const publishVue2AppOnline = () => {
+  return new Promise((resolve) => {
+    const buildCommand = `cd pages && npm run build:vue2`;
+    shelljs.exec(buildCommand, () => {
+      const syncCommand = `npm publish --registry=https://registry.npmjs.org && node sync.js --origin=https://my.mybricks.world vue2 ${noServiceUpdate ? "--noServiceUpdate" : ""
+        }`;
+      shelljs.exec(syncCommand, resolve);
+    });
+  })
+
 };
 
 const fixAppInfo = () => {
-  const json = { ...pkgJson };
-  json.name = json.appConfig.vue2.name;
-  json.mybricks = { ...json.mybricks, ...json.appConfig.vue2.mybricks };
-  fs.writeFileSync("./package.json", JSON.stringify(json, null, 2));
+  return new Promise((resolve) => {
+    const json = { ...pkgJson };
+    json.name = json.appConfig.vue2.name;
+    json.mybricks = { ...json.mybricks, ...json.appConfig.vue2.mybricks };
+    fs.writeFileSync("./package.json", JSON.stringify(json, null, 2));
+    resolve()
+  })
 };
 
 const resetPkg = () => {
   fs.writeFileSync("./package.json", JSON.stringify(pkgJson, null, 2));
 };
 
-if (isOffline) {
-  publishReactAppOffline(function () {
-    fixAppInfo();
-    publishVue2AppOffline(resetPkg);
-  });
-} else {
-  publishReactAppOnline(function () {
-    fixAppInfo();
-    publishVue2AppOnline(resetPkg);
-  });
+const execChain = (fns) => {
+  fns.reduce((chain, fn) => chain.then(fn), Promise.resolve())
 }
+
+if (isOffline) {
+  execChain([publishReactAppOffline, fixAppInfo, publishVue2AppOffline, resetPkg])
+} else {
+  execChain([publishReactAppOnline, fixAppInfo, publishVue2AppOnline, resetPkg])
+}
+
+
