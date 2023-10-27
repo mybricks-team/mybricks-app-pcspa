29305715466d074bd7f4ec8678726695cafbe782
jidan.wd <wudi27@kuaishou.com>
2023-10-27 16:56:23 +0800

    feat: 补充注释

--- nodejs/module/service/rollback.ts
+++ nodejs/module/service/rollback.ts
@@ -6,7 +6,7 @@ import { publishPush } from "./publish/push";
 
 export async function rollback(req: any, filePath: string) {
   // TODO: 回滚重试机制
-  // TODO: 优化压缩包体积
+  // TODO: 优化压缩包体积，可以和发布集成推送数据大小优化一起，不用单独优化
 
   try {
     const domainName =
