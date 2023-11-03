dcdba728994f713b2e35b1635820613121ea919b
tangxiaoxin <tangxiaoxin@kuaishou.com>
2023-11-03 11:03:10 +0800

    upgrade: render-web-vue2@0.0.2

--- publish.js
+++ publish.js
@@ -1,12 +1,15 @@
 const shelljs = require("shelljs");
 const pkgJson = require("./package.json");
 const fs = require("fs");
+const path = require('path')
 const args = process.argv.slice(2);
 const isOffline = !!args.find((a) => a === "offline");
 const noServiceUpdate = !!args.find((a) => a === "--noServiceUpdate");
 const origin =
   args.find((a) => a.startsWith("--origin=")) ??
   "--origin=https://my.mybricks.world";
+
+const ClearFiles = ['mybricks-app-pcspa', 'mybricks-app-pcspa-vue2']
 
 const publishReactAppOffline = () => {
   return new Promise((resolve) => {
@@ -32,9 +35,8 @@ const publishReactAppOnline = () => {
   return new Promise((resolve) => {
     const buildCommand = `cd pages && npm run build:react`;
     shelljs.exec(buildCommand, () => {
-      const syncCommand = `npm publish --registry=https://registry.npmjs.org && node sync.js ${origin} --appType=react ${
-        noServiceUpdate ? "--noServiceUpdate" : ""
-      }`;
+      const syncCommand = `npm publish --registry=https://registry.npmjs.org && node sync.js ${origin} --appType=react ${noServiceUpdate ? "--noServiceUpdate" : ""
+        }`;
       shelljs.exec(syncCommand, resolve);
     });
   });
@@ -44,9 +46,8 @@ const publishVue2AppOnline = () => {
   return new Promise((resolve) => {
     const buildCommand = `cd pages && npm run build:vue2`;
     shelljs.exec(buildCommand, () => {
-      const syncCommand = `npm publish --registry=https://registry.npmjs.org && node sync.js ${origin} --appType=vue2 ${
-        noServiceUpdate ? "--noServiceUpdate" : ""
-      }`;
+      const syncCommand = `npm publish --registry=https://registry.npmjs.org && node sync.js ${origin} --appType=vue2 ${noServiceUpdate ? "--noServiceUpdate" : ""
+        }`;
       shelljs.exec(syncCommand, resolve);
     });
   });
@@ -66,12 +67,23 @@ const resetPkg = () => {
   fs.writeFileSync("./package.json", JSON.stringify(pkgJson, null, 2));
 };
 
+const clearZipPkg = () => {
+  const dirPath = path.resolve(__dirname)
+  const files = fs.readdirSync(dirPath)
+  files.forEach(file => {
+    if (ClearFiles.some(filename => file.includes(filename)) && file.endsWith('.zip')) {
+      fs.unlinkSync(path.join(dirPath, file));
+    }
+  })
+}
+
 const execChain = (fns) => {
   fns.reduce((chain, fn) => chain.then(fn), Promise.resolve());
 };
 
 if (isOffline) {
   execChain([
+    clearZipPkg,
     publishReactAppOffline,
     fixPkg,
     publishVue2AppOffline,
