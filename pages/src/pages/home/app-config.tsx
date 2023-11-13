19d22a444a3fde579d38351e8f2921652d68c12e
liuzhigang06 <liuzhigang06@kuaishou.com>
2023-11-13 19:53:50 +0800

    chore: update locale version

--- pages/src/pages/home/app-config.tsx
+++ pages/src/pages/home/app-config.tsx
@@ -7,8 +7,8 @@ import servicePlugin, {
 import domainServicePlugin, { call as callDomainHttp } from '@mybricks/plugin-connector-domain'
 // import { openFilePanel } from "@mybricks/sdk-for-app/ui";
 import versionPlugin from 'mybricks-plugin-version'
-import localePlugin from '../../../../../plugin-locale/src'
-// import localePlugin from 'mybricks-plugin-locale'
+// import localePlugin from '../../../../../plugin-locale/src'
+import localePlugin from 'mybricks-plugin-locale'
 import { use as useTheme } from '@mybricks/plugin-theme';
 
 import { render as renderUI } from '@mybricks/render-web';
