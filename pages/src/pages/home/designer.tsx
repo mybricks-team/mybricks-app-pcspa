76fb0ab6a51269cb72c02b70a30e9b9ff570d3b2
liuzhigang06 <liuzhigang06@kuaishou.com>
2023-11-27 21:22:37 +0800

    Revert "chore: update designer"
    This reverts commit df1d68eec072a39b6784d73c95b067a7fa9b5063.

--- pages/src/pages/home/designer.tsx
+++ pages/src/pages/home/designer.tsx
@@ -69,7 +69,7 @@ export default function MyDesigner({ appData: originAppData }) {
     }
   }
 
-  const designer = './public/designer-spa/1.3.65/index.min.js'
+  const designer = './public/designer-spa/1.3.63/index.min.js'
 
   const appConfig = useMemo(() => {
     let config = null
