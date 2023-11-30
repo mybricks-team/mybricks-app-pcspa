7e054a9097573faa846c20c3a94ecfed3efcedea
GodStream <zpqwangyiyouxiang@163.com>
2023-11-30 22:12:57 +0800

    chore: upgrade

--- pages/src/pages/home/designer.tsx
+++ pages/src/pages/home/designer.tsx
@@ -69,7 +69,7 @@ export default function MyDesigner({ appData: originAppData }) {
     }
   }
 
-  const designer = './public/designer-spa/1.3.66/index.min.js'
+  const designer = './public/designer-spa/1.3.66-v1/index.min.js'
 
   const appConfig = useMemo(() => {
     let config = null
