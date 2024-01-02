64d9706c0e188dcb922623fe52fd992f872635d3
wudi27 <wudi27@kuaishou.com>
2024-01-02 21:01:37 +0800

    feat: v1.2.51

--- pages/src/pages/home/designer.tsx
+++ pages/src/pages/home/designer.tsx
@@ -25,7 +25,7 @@ import { USE_CUSTOM_HOST } from './constants'
 import { getLibsFromConfig } from '../../utils/getComlibs'
 
 const msgSaveKey = 'save'
-const designer = './public/designer-spa/1.3.75/index.min.js'
+const designer = './public/designer-spa/1.3.76/index.min.js'
 
 /**
  * @description 获取当前应用setting
