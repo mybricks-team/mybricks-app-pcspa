93fe20c5c65b9f19c6033c69ab72749c6a92aefc
liuzhigang06 <liuzhigang06@kuaishou.com>
2024-01-18 11:01:29 +0800

    chore: update verison

--- pages/src/pages/home/designer.tsx
+++ pages/src/pages/home/designer.tsx
@@ -26,7 +26,7 @@ import { getLibsFromConfig } from '../../utils/getComlibs'
 import { proxLocalStorage, proxSessionStorage } from '@/utils/debugMockUtils'
 
 const msgSaveKey = 'save'
-const designer = './public/designer-spa/1.3.85.11/index.min.js'
+const designer = './public/designer-spa/1.3.86/index.min.js'
 
 /**
  * @description 获取当前应用setting
