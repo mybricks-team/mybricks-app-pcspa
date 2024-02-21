cb19ffbdf6e9623061d4d2ca8e3fc579d3704822
GodStream <zpqwangyiyouxiang@163.com>
2024-02-21 22:23:33 +0800

    chore: upgrade designer-spa

--- pages/src/constants/index.ts
+++ pages/src/constants/index.ts
@@ -53,4 +53,4 @@ export const MySelf_COM_LIB = {
 };
 
 
-export const DESIGNER_STATIC_PATH = './public/designer-spa/1.3.97.7/index.min.js'
+export const DESIGNER_STATIC_PATH = './public/designer-spa/1.4.3.2/index.min.js'
