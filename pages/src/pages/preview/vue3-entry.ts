89bb4c94a97ce427f5655081382f4fb17795b7dd
tangxiaoxin <1302947749@qq.com>
2024-02-26 14:06:05 +0800

    vue3

---
+++ pages/src/pages/preview/vue3-entry.ts
@@ -0,0 +1,70 @@
+import { getQueryString, requireScript } from "@/utils";
+import { PreviewStorage } from "@/utils/previewStorage";
+import renderUI from "./renderUI";
+import "@/reset.less";
+const fileId = getQueryString("fileId");
+const previewStorage = new PreviewStorage({ fileId });
+
+let { dumpJson, comlibs } = previewStorage.getPreviewPageData();
+const Vue = window.Vue;
+let vueApp;
+
+if (!dumpJson) {
+  throw new Error("数据错误：项目数据缺失");
+}
+
+function cssVariable(dumpJson) {
+  const themes = dumpJson?.plugins?.["@mybricks/plugins/theme/use"]?.themes;
+  if (Array.isArray(themes)) {
+    themes.forEach(({ namespace, content }) => {
+      const variables = content?.variables;
+
+      if (Array.isArray(variables)) {
+        const style = document.createElement("style");
+        style.id = namespace;
+        let innerHTML = "";
+
+        variables.forEach(({ configs }) => {
+          if (Array.isArray(configs)) {
+            configs.forEach(({ key, value }) => {
+              innerHTML = innerHTML + `${key}: ${value};\n`;
+            });
+          }
+        });
+
+        style.innerHTML = `:root {\n${innerHTML}}`;
+        document.body.appendChild(style);
+      }
+    });
+  }
+}
+
+cssVariable(dumpJson);
+
+const render = (props) => {
+  const { container } = props;
+  if (comlibs && Array.isArray(comlibs)) {
+    Promise.all(comlibs.map((t) => requireScript(t))).then(() => {
+      vueApp = Vue.createApp(renderUI({ ...props, renderType: "vue3" })).mount(
+        (container ?? document).querySelector("#root")
+      );
+    });
+  }
+};
+
+if (!window.__POWERED_BY_QIANKUN__) {
+  render({});
+}
+
+export async function bootstrap() {
+  console.log("react app bootstrap");
+}
+
+export async function mount(props) {
+  render(props);
+}
+
+export async function unmount(props) {
+  vueApp.unmount();
+  vueApp.$el.innerHTML = "";
+}
