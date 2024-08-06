8ef990e9e08fc4a2f5cd1f1cb216e29768a1c3c4
GodStream <zpqwangyiyouxiang@163.com>
2024-08-06 17:53:55 +0800

    feat: 增加流式接口

--- pages/src/pages/home/app-config.tsx
+++ pages/src/pages/home/app-config.tsx
@@ -1416,6 +1416,38 @@ const getAiView = (enableAI) => {
           // 返回结果: ${content}`)
         }
       },
+      async requestAsStream(messages, { write, complete, error }) {
+        try {
+          const response = await fetch('//ai.mybricks.world/stream', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(
+              getAiEncryptData({
+                messages,
+              })
+            ),
+          })
+
+          const reader = response.body.getReader()
+          const decoder = new TextDecoder()
+
+          while (true) {
+            const { done, value } = await reader.read()
+            if (done) {
+              break
+            }
+
+            const chunk = decoder.decode(value, { stream: true })
+            write(chunk)
+          }
+
+          complete()
+        } catch (ex) {
+          error(ex)
+        }
+      },
     }
   }
 
