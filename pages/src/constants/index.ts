6f9ec2cecbb5ff5adfc905ad0958902bcb9e33ed
leon-js <42924541+leon-js@users.noreply.github.com>
2024-12-02 17:27:30 +0800

    feat:更新引擎和渲染器

--- pages/src/constants/index.ts
+++ pages/src/constants/index.ts
@@ -55,4 +55,4 @@ export const MySelf_COM_LIB = {
 };
 
 //调试时指向test/my的设计器文件
-export const DESIGNER_STATIC_PATH = './public/designer-spa/3.9.022/index.min.js'
+export const DESIGNER_STATIC_PATH = './public/designer-spa/3.9.025/index.min.js'
