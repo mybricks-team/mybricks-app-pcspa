77264309ed0048242c86656091357699a3e1c8ad
leon-js <42924541+leon-js@users.noreply.github.com>
2025-03-06 19:27:18 +0800

    feat:更新渲染器版本

--- nodejs/module/service/local-public.ts
+++ nodejs/module/service/local-public.ts
@@ -67,8 +67,8 @@ export default {
     },
     {
       tag: "script",
-      path: "public/render-web/1.3.50/index.min.js",
-      CDN: "//f2.beckwai.com/kos/nlav12333/mybricks/render-web/1.3.50/index.min.js",
+      path: "public/render-web/1.3.51/index.min.js",
+      CDN: "//f2.beckwai.com/kos/nlav12333/mybricks/render-web/1.3.51/index.min.js",
     },
   ],
   vue2: [
