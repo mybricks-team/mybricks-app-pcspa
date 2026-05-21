eadd5fa7bae8747184438cb957e654131c6800d4
liubiao03 <liubiao03@kuaishou.com>
2026-05-21 22:53:21 +0800

    feat:update

--- pages/src/pages/design/app-configs/index.tsx
+++ pages/src/pages/design/app-configs/index.tsx
@@ -544,7 +544,7 @@ export default function appConfig(
           title: "PC-AI组件库",
           type: "com_lib",
           namespace: "mybricks.normal-pc-lite",
-          editJs: 'https://p4-ec.ecukwai.com/kos/nlav11092/vibe-coding/comlib/2.0.39/edit.js',
+          editJs: 'https://p4-ec.ecukwai.com/kos/nlav11092/vibe-coding/comlib/2.0.52/edit.js',
         };
         const existing = baseComlibs.find(
           (lib) => lib.namespace === pcAiComlib.namespace && lib.editJs
