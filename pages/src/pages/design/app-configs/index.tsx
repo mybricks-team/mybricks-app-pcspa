94f7aa33e44fc07c49771ab5529b7596478e90a1
patrick <786156072@qq.com>
2026-07-08 16:29:59 +0800

    fix: local

--- pages/src/pages/design/app-configs/index.tsx
+++ pages/src/pages/design/app-configs/index.tsx
@@ -546,7 +546,7 @@ export default function appConfig(
           title: "PC-AI组件库",
           type: "com_lib",
           namespace: "mybricks.normal-pc-lite",
-          editJs: 'https://p4-ec.ecukwai.com/kos/nlav11092/vibe-coding/comlib/2.0.88/edit.js',
+          editJs: 'public/vibe-coding/2.0.88/edit.js',
         };
         const existing = baseComlibs.find(
           (lib) => lib.namespace === pcAiComlib.namespace && lib.editJs
